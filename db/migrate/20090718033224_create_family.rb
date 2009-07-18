3b1ccd14f9713eaf8bcb2d83093a3655eca16e67
ralf user <ralf@kim-laptop.(none)>
2009-07-17 23:44:04 -0400

    Create migrations for participants, their families, and the rooms they're going to stay in.

---
+++ db/migrate/20090718033224_create_family.rb
@@ -0,0 +1,13 @@
+class CreateFamily < ActiveRecord::Migration
+  def self.up
+    create_table :families, :force => true do |f|
+      f.column :principal, :string
+      f.column :photo, :string
+      f.column :familyname, :string
+      f.column :note, :string
+  end
+
+  def self.down
+    drop_table :families
+  end
+end
