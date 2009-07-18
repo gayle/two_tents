eeb12f7fee91bdf013c060a4f62375eeb0bff5f8
Kim Toms <kim.toms@gmail.com>
2009-07-18 19:08:57 -0400

    Add ability to upload and attach photos to families

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,6 +1,8 @@
 class Family < ActiveRecord::Base
   has_many :participants 
 
+  has_attached_file :photograph
+
   # Alias method 'members' to 'participants'.  Use either one.
   def members
     participants
