0552353dabd8d3ab66417878f3bd6be48ee9d88e
Todd Kaufman <toddkaufman@gmail.com>
2009-07-18 16:06:14 -0400

    - Updated participants to find only non-staff for drop down - Added listing for staff

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,14 @@
 class Participant < ActiveRecord::Base
   belongs_to :family 
   belongs_to :user
+  
+  def fullname
+    "#{firstname} #{lastname}"
+  end
+
+  def self.find_non_staff_participants
+    Participant.all.reject do |p|
+      p.user
+    end
+  end
 end
--- test/unit/participant_test.rb
+++ test/unit/participant_test.rb
@@ -1,8 +1,31 @@
 require 'test_helper'
 
 class ParticipantTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+
+fixtures :participants
+
+    def test_should_find_non_users
+      nu = participants(:non_user)
+      nu.save
+      
+      non_staff = Participant.find_non_staff_participants
+      assert_equal(1, non_staff.size)
+      assert_equal nu, non_staff[0]
+    end
+    
+    def test_should_not_find_existing_users
+      staff = create_user
+      non_staff = Participant.find_non_staff_participants
+      assert_equal(1, non_staff.size)
+      assert_not_equal staff, non_staff[0]
+    end
+
+  protected
+    def create_user
+      record = User.new({ :login => 'quire', :email => 'quire@example.com',
+                          :password => 'quire69', :password_confirmation => 'quire69' })
+      record.participant = participants(:quentin)
+      record.save
+      record
+    end
 end
