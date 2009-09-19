bfb4882b84d0cdf5c12b46c892955573a522ff0d
gayle <gayle@craigworld.com>
2009-09-19 00:18:16 -0400

    added registration stats to header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def registration_stats
+    num_families = Family.find(:all).size
+    num_participants = Participant.find(:all).size
+    "#{num_families} families, #{num_participants} participants."
+  end
+  
   def get_messages
     if ! flash[:error].blank?
       create_message_tag(:error)
