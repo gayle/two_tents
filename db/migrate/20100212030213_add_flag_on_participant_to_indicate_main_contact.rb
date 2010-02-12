6e583c985141693dae17f73e6851824bb92a72cf
gayle <gayle_999@yahoo.com>
2010-02-11 23:50:33 -0500

    Migration to flag participant as primary member

---
+++ db/migrate/20100212030213_add_flag_on_participant_to_indicate_main_contact.rb
@@ -0,0 +1,9 @@
+class AddFlagOnParticipantToIndicateMainContact < ActiveRecord::Migration
+  def self.up
+    add_column :participants, :main_contact, :boolean, :default => false
+  end
+
+  def self.down
+    remove_column :participants, main_contact
+  end
+end
