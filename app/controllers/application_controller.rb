a6e3ed639bfff81415199fb84e59ec57ed23fa63
gayle <gayle_999@yahoo.com>
2010-02-11 23:06:14 -0500

    more to try to figure out why roles don't work in prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # AuthenticatedSystem must be included for RoleRequirement, and is provided by installing acts_as_authenticates and running 'script/generate authenticated account user'.
   include AuthenticatedSystem
   # You can move this into a different controller, if you wish.  This module gives you the require_role helpers, and others.
-  include RoleRequirementSystem
+  #include RoleRequirementSystem
 
 
   helper :all # include all helpers, all the time
