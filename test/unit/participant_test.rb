e22d5455dee09c84910d18700c3dfd54928cfce7
gregmalcolm <greg@gregmalcolm.com>
2010-07-10 17:28:32 -0400

    Working on duplication issues; Did something about some participant tests I acidentally checked in commented out

--- test/unit/participant_test.rb
+++ test/unit/participant_test.rb
@@ -17,39 +17,29 @@ class ParticipantTest < ActiveSupport::TestCase
       assert_equal(10, non_staff.size)
     end
 
-#    def test_should_not_find_existing_users
-#      Factory(:user)
-#      non_staff = Participant.find_non_staff_participants
-#      assert_equal(9, non_staff.size)
-#    end
-#
-#    def test_fullname_should_handle_firstname_being_nil
-#      p = Factory(:participant, :firstname => nil)
-#
-#      verify_fullname_is_unharmed_by_nulls p
-#    end
-#
-#    def test_fullname_should_handle_lastname_being_nil
-#      p = Factory(:participant, :lastname => nil)
-#
-#      verify_fullname_is_unharmed_by_nulls p
-#    end
-#
-#    def test_fullname_should_handle_both_names_being_nil
-#      p = Factory(:participant, :firstname => nil, :lastname => nil)
-#
-#      verify_fullname_is_unharmed_by_nulls p
-#    end
-#
-#    def test_participant_cannot_be_destroyed_if_it_belongs_to_a_user
-#      u = Factory(:user)
-#      u.destroy
-#      assert_equal 19, Participant.all.size
-#    end
+    def test_should_not_find_existing_users
+      Factory(:user)
+      non_staff = Participant.find_non_staff_participants
+      assert_equal(9, non_staff.size)
+    end
 
-  private
-    def verify_fullname_is_unharmed_by_nulls(participant)
-      assert_not_nil participant.fullname, "resulting fullname was nil"
-      assert !participant.fullname.include?("nil"), "the string fullname shouldn't contain the word nil if the value is nil"
+    def test_firstname_should_not_nil
+      p = Factory.build(:participant, :firstname => nil)
+      p.save
+
+      assert p.errors.on(:firstname)
+    end
+
+    def test_lastname_should_not_nil
+      p = Factory.build(:participant, :lastname => nil)
+      p.save
+
+      assert p.errors.on(:lastname)
+    end
+
+    def test_participant_cannot_be_destroyed_if_it_belongs_to_a_user
+      u = Factory(:user)
+      u.destroy
+      assert_equal 19, Participant.all.size
     end
 end
