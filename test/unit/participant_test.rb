833432c23ad2ec0596ea6d224bce60a96a2cee69
Matt Jones <al2o3cr@gmail.com>
2010-07-10 16:53:17 -0400

    better age calculation - fix #96

--- test/unit/participant_test.rb
+++ test/unit/participant_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class ParticipantTest < ActiveSupport::TestCase
 
+    def setup
+      @camp_start = Date.new(2010,7,21)
+    end
+
     def test_there_18_users_created_by_fixtures
       assert_equal 18, Participant.all.size
     end
@@ -15,6 +19,41 @@ class ParticipantTest < ActiveSupport::TestCase
 
       non_staff = Participant.find_non_staff_participants
       assert_equal(10, non_staff.size)
+    end
+
+    def test_under_one_month
+      p = Participant.new(:birthdate => @camp_start.advance(:days => -10))
+      assert_equal [0,0,10], p.age_parts
+      assert_equal 0, p.age
+      assert_equal "10 days", p.display_age
+    end
+
+    def test_under_one_year
+      p = Participant.new(:birthdate => @camp_start.advance(:months => -9, :days => -21))
+      assert_equal [0,9,21], p.age_parts
+      assert_equal 0, p.age
+      assert_equal "9 months", p.display_age
+    end
+
+    def test_one_year_edge_case
+      p = Participant.new(:birthdate => @camp_start.advance(:months => -11, :days => -29))
+      assert_equal [0,11,29], p.age_parts
+      assert_equal 0, p.age
+      assert_equal "11 months", p.display_age
+    end
+
+    def test_over_one_year
+      p = Participant.new(:birthdate => @camp_start.advance(:years => -1, :months => -10, :days => -10))
+      assert_equal [1,10,10], p.age_parts
+      assert_equal 1, p.age
+      assert_equal "22 months", p.display_age
+    end
+
+    def test_over_ten_years
+      p = Participant.new(:birthdate => @camp_start.advance(:years => -10, :months => -10, :days => -10))
+      assert_equal [10,10,10], p.age_parts
+      assert_equal 10, p.age
+      assert_equal "10", p.display_age
     end
 
 #    def test_should_not_find_existing_users
