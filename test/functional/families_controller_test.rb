6874ed8991136edb979a078642dd6ab7d6c907c4
gayle <gayle_999@yahoo.com>
2010-07-11 05:08:25 -0400

    Changed families fixtures to FactoryGirl stuff

--- test/functional/families_controller_test.rb
+++ test/functional/families_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class FamiliesControllerTest < ActionController::TestCase
   include AuthenticatedTestHelper
 
-  fixtures :users
+#  fixtures :users, :families, :participants
 
   def setup
     login_as :quentin
@@ -15,38 +15,54 @@ class FamiliesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:families)
   end
 
+  test "should sort properly when more than will fit on one page" do
+    Factory(:family)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
 
   test "should create families" do
-    assert_difference('Family.count') do
-      post :create, :commit => 'Save', :family => {:participants_attributes => {
-                                                     '0' => { :firstname => 'Foo', :lastname => 'Foo', :birthdate => 10.years.ago.to_date, :state => 'OH', :main_contact => '1'},
-                                                     '1' => { :firstname => 'Bar', :lastname => 'Bar', :birthdate => 10.years.ago.to_date, :state => 'OH', :main_contact => '0'}
+    post :create, :commit => 'Save', :family => {:participants_attributes => {
+                                                   '0' => { :firstname => 'Foo', :lastname => 'Foo', :birthdate => 10.years.ago.to_date, :state => 'OH', :main_contact => '1'},
+                                                   '1' => { :firstname => 'Bar', :lastname => 'Bar', :birthdate => 10.years.ago.to_date, :state => 'OH', :main_contact => '0'}
                                                   }}
-      assert_redirected_to families_path
-    end
+    assert_redirected_to families_path
+    assert_equal 1, Family.count
     p = Participant.find_by_firstname_and_lastname('Foo','Foo')
-    assert_equal p, p.family.main_contact
     assert_equal 2, p.family.participants.size
   end
 
   test "should get edit" do
-    get :edit, :id => families(:space).to_param
+    f = Factory(:family)
+    get :edit, :id => f.to_param
     assert_response :success
   end
 
   test "should update families" do
-    put :update, :id => families(:space).to_param, :family => { :participants_attributes => { '12345' => { :firstname => 'Blah', :id => 1 }}}
-    assert families(:space).participants(true).find_by_firstname('Blah')
+    p = Factory(:participant)
+    f = Factory(:family, :participants => [p])
+
+    participant_attrs = { '12345' => { :firstname => 'Foo', :lastname => 'Bar', :birthdate => "02/04/1975" }}
+    put :update, :id => f.to_param, :family => { :participants_attributes => participant_attrs }
+
+    assert Participant.find_by_firstname('Foo')
     assert_redirected_to families_path
   end
 
   test "should destroy families" do
+    5.times do
+      Factory(:family)
+    end
+    f = Factory(:family, :familyname => "space")
+    5.times do
+      Factory(:family)
+    end
+
     assert_difference('Family.count', -1) do
-      delete :destroy, :id => families(:space).to_param
+      delete :destroy, :id => f.to_param
     end
 
     assert_redirected_to families_path
