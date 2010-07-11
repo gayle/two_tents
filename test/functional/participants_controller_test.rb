d4df0028663f31e9620a9f807a66501cc16d7e98
Matt Jones <al2o3cr@gmail.com>
2010-07-11 12:29:45 -0400

    fix broken test

--- test/functional/participants_controller_test.rb
+++ test/functional/participants_controller_test.rb
@@ -60,8 +60,8 @@ class ParticipantsControllerTest < ActionController::TestCase
   end
 
   test "should destroy participants without a user" do
+    p = Factory(:participant)
     assert_difference('Participant.count', -1) do
-      p = Factory(:participant)
       delete :destroy, :id => p.to_param
     end
 
