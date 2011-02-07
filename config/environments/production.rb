b706e30913fd78bbf64986e5d22646c5d3fd429b
gayle <gayle@craigworld.com>
2011-02-07 00:42:26 -0500

    changed production email address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,4 +27,4 @@ config.action_view.cache_template_loading            = true
 # Enable threaded mode
 # config.threadsafe!
 
-CONTACT_EMAIL = "gayle@craigworld.com"
+CONTACT_EMAIL = "familycampstaff@gmail.com"
