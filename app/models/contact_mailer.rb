d5ff7f53ee5cca5598283f5916b0efe4ab7b8dfd
gayle <gayle@craigworld.com>
2011-02-07 23:07:38 -0500

    Changed email address for development, and subject line of "contact us" email

--- app/models/contact_mailer.rb
+++ app/models/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ActionMailer::Base
   def contact_email(contact)
     recipients CONTACT_EMAIL
     from       "#{contact.name} <#{contact.email}>"
-    subject    "[familycamp CONTACT] #{contact.subject}"
+    subject    "[A message from the family camp website!] #{contact.subject}"
     sent_on    Time.now
     body       "#{contact.comment}"
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,4 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-CONTACT_EMAIL = "busch.michael@gmail.com"
+CONTACT_EMAIL = "gayle_999@yahoo.com"
