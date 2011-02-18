68c1ce534faefc968ff159a432022a4511854e23
Mike Busch <busch.michael@gmail.com>
2011-02-17 23:37:59 -0600

    fix bundler issues

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,7 +15,7 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "bundler"
+  # config.gem "bundler"
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
@@ -42,10 +42,10 @@ Rails::Initializer.run do |config|
 
 # ?? It seems like I shouldn't need these two since they're in the Gemfile, but app doesn't work w/o these here. 
   # Use will_paginate
-  config.gem 'mislav-will_paginate', :version => '~> 2.3.8', :lib => 'will_paginate',
-    :source => 'http://gems.github.com'
+#  config.gem 'mislav-will_paginate', :version => '~> 2.3.8', :lib => 'will_paginate',
+#    :source => 'http://gems.github.com'
 
   # should this move to environments/production.rb ?
-  config.gem 'hoptoad_notifier'
+#  config.gem 'hoptoad_notifier'
   
 end
