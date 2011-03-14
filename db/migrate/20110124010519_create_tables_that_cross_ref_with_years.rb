5f33fcbc5898b3c531c7db895dfa169618008f85
gayle <gayle@craigworld.com>
2011-03-14 02:39:05 -0400

    Fixed bug in migration that would mess up prod data (found when deploying changes to demo)

--- db/migrate/20110124010519_create_tables_that_cross_ref_with_years.rb
+++ db/migrate/20110124010519_create_tables_that_cross_ref_with_years.rb
@@ -1,7 +1,7 @@
 class CreateTablesThatCrossRefWithYears < ActiveRecord::Migration
   def self.up
-    year_results = execute("SELECT * FROM years ORDER BY year ASC")
-    oldest_year_id = year_results[0]["id"]
+    # year_results = execute("SELECT * FROM years ORDER BY year ASC")
+    year_2010_id = execute("SELECT * FROM years WHERE year = '2010'")[0].id # year_results[0]["id"]
 
     create_table :families_years, :id => false do |t|
       t.integer :family_id
@@ -10,7 +10,7 @@ class CreateTablesThatCrossRefWithYears < ActiveRecord::Migration
     end
     # initially, have every family use a previous year
     execute("SELECT id FROM families").each do |family|
-      insert %{INSERT INTO families_years (family_id, year_id) VALUES('#{family["id"]}','#{oldest_year_id}')}
+      insert %{INSERT INTO families_years (family_id, year_id) VALUES('#{family["id"]}','#{year_2010_id}')}
     end
 
     create_table :participants_years, :id => false do |t|
@@ -20,7 +20,7 @@ class CreateTablesThatCrossRefWithYears < ActiveRecord::Migration
     end
     # initially, have every participant use a previous year
     execute("SELECT id FROM participants").each do |participant|
-      insert %{INSERT INTO participants_years (participant_id, year_id) VALUES('#{participant["id"]}','#{oldest_year_id}')}
+      insert %{INSERT INTO participants_years (participant_id, year_id) VALUES('#{participant["id"]}','#{year_2010_id}')}
     end
   end
 
