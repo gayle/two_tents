9dbd6ea09134c2f23973f83dacc6f2c8c6a10072
Tyler Wymer <twymer@gmail.com>
2011-05-21 00:38:07 -0400

    Family name on view participant page, move/add verbage.

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -1,3 +1,11 @@
 module ParticipantsHelper
+  def existing_family_text
+    prefix = @participant.family ? "Move" : "Add" 
+    prefix + " This Person To An Existing Family"
+  end
 
+  def new_family_text
+    prefix = @participant.family ? "Move" : "Add" 
+    prefix + " This Person To A New Family"
+  end
 end
