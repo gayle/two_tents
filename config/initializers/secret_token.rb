4a4c6e2acbeb51465fe5f762d971ee95b4990e2f
Gayle Bender <gayle_999@yahoo.com>
2013-08-31 22:35:18 -0400

    Forgot to add new files as part of rails 3 project generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwoTents::Application.config.secret_token = 'e8e33390cfddcfa8ecc518a70da6bef08b1fde47afe891e1442f48a81f57384d3aa3ba6850b3cd60ac9d6ead5fff5e7cbe79149c721be973637009a7fb0c8a59'
