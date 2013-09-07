e882f00ef8337ed792605f8897aa8c004a8cb504
Gayle Bender <gayle_999@yahoo.com>
2013-09-07 17:39:18 -0400

    Moved methods from authenticated_test_helper from lib into test_helper.rb b/c the test didn't know of things in the lib directory

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,6 @@ module TwoTents
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Add additional load paths for your own custom dirs
-    # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
