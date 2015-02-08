2fd2d2c478ca936e8119edafaca868e02e8f1134
Gayle Bender <gayle_999@yahoo.com>
2015-02-08 18:31:23 -0500

    more test refactoring

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -12,11 +12,11 @@ RSpec.describe Participant, :type => :model do
     before do
       @this_year = FactoryGirl.create(:year, year: 2015)
       @last_year = FactoryGirl.create(:year, year: 2014)
-      @adam = FactoryGirl.create(:participant, firstname: "Adam", lastname: "Apple", years: [@last_year, @this_year])
-      @bill = FactoryGirl.create(:participant, firstname: "Bill", lastname: "Banana", years: [@last_year])
-      @chad = FactoryGirl.create(:participant, firstname: "Chad", lastname: "Cherry", years: [@this_year])
-      #@bill.years = [@last_year] # remove current year that gets added by default in after_initialize. They have only past year.
-      #@bill.save!
+      @someone_registered_last_and_this_year = FactoryGirl.create(:participant, firstname: "Adam", lastname: "Apple", years: [@last_year, @this_year])
+      @someone_registered_last_year_only = FactoryGirl.create(:participant, firstname: "Bill", lastname: "Banana", years: [@last_year])
+      @someone_registered_this_year_only = FactoryGirl.create(:participant, firstname: "Chad", lastname: "Cherry", years: [@this_year])
+      #@someone_registered_last_year_only.years = [@last_year] # remove current year that gets added by default in after_initialize. They have only past year.
+      #@someone_registered_last_year_only.save!
     end
 
     # Copied after_initialize over from old rails 2, but it seems to cause more problems than it solves.  Taking it out until I find a good use case for it.
@@ -30,50 +30,50 @@ RSpec.describe Participant, :type => :model do
 
     context ".registered" do
       it "should be able to list current participants" do
-        expect(Participant.registered).to include @adam
-        expect(Participant.registered).to include @chad
-        expect(Participant.registered).not_to include @bill
+        expect(Participant.registered).to include @someone_registered_last_and_this_year
+        expect(Participant.registered).to include @someone_registered_this_year_only
+        expect(Participant.registered).not_to include @someone_registered_last_year_only
       end
     end
 
     context ".not_registered" do
       it "should be able to list past participants that are not registered this year" do
-        expect(Participant.not_registered).not_to include @adam
-        expect(Participant.not_registered).to include @bill
-        expect(Participant.not_registered).not_to include @chad
+        expect(Participant.not_registered).not_to include @someone_registered_last_and_this_year
+        expect(Participant.not_registered).to include @someone_registered_last_year_only
+        expect(Participant.not_registered).not_to include @someone_registered_this_year_only
       end
     end
 
     context "#full_name" do
       it "should display first name then last name" do
-        @adam = FactoryGirl.create(:participant, firstname: "Adam", lastname: "Apple", years: [@last_year, @this_year])
-        expect(@adam.full_name).to eq "Adam Apple"
+        @someone_registered_last_and_this_year = FactoryGirl.create(:participant, firstname: "Adam", lastname: "Apple", years: [@last_year, @this_year])
+        expect(@someone_registered_last_and_this_year.full_name).to eq "Adam Apple"
       end
     end
 
     context "#list_name" do
       it "should display last name then first name" do
-        expect(@adam.list_name).to eq "Apple, Adam"
+        expect(@someone_registered_last_and_this_year.list_name).to eq "Apple, Adam"
       end
     end
 
     context "#registered?" do
       it "should determine whether participants are registered" do
-        expect(@adam.registered?(@this_year)).to eq true
-        expect(@bill.registered?(@this_year)).to eq false
-        expect(@chad.registered?(@this_year)).to eq true
+        expect(@someone_registered_last_and_this_year.registered?(@this_year)).to eq true
+        expect(@someone_registered_last_year_only.registered?(@this_year)).to eq false
+        expect(@someone_registered_this_year_only.registered?(@this_year)).to eq true
       end
     end
 
     context "#full_name" do
       it "should concatenate full name with first name first" do
-        expect(@adam.full_name).to eq "Adam Apple"
+        expect(@someone_registered_last_and_this_year.full_name).to eq "Adam Apple"
       end
     end
 
     context "#list_name" do
       it "should concatenate list name with last name first" do
-        expect(@adam.list_name).to eq "Apple, Adam"
+        expect(@someone_registered_last_and_this_year.list_name).to eq "Apple, Adam"
       end
     end
   end
@@ -94,33 +94,33 @@ RSpec.describe Participant, :type => :model do
     before do
       @this_year = FactoryGirl.create(:year, year: 2015)
       @last_year = FactoryGirl.create(:year, year: 2014)
-      @elderberry = FactoryGirl.create(:participant, firstname: "Barry", lastname: "Elder")
+      @someone = FactoryGirl.create(:participant, firstname: "Barry", lastname: "Elder")
     end
 
     it "should register for current year" do
-      expect(@elderberry.years).not_to include @this_year
+      expect(@someone.years).not_to include @this_year
 
-      @elderberry.register
+      @someone.register
 
-      expect(@elderberry.years).to include @this_year
+      expect(@someone.years).to include @this_year
     end
 
     it "should not register for that year twice, no matter how many times you call register" do
-      @elderberry = FactoryGirl.create(:participant, firstname: "Barry", lastname: "Elder")
+      @someone = FactoryGirl.create(:participant, firstname: "Barry", lastname: "Elder")
 
-      @elderberry.register
-      @elderberry.register
+      @someone.register
+      @someone.register
 
-      expect(@elderberry.years.size).to eq 1
+      expect(@someone.years.size).to eq 1
     end
 
     it "should be able to register for a year other than current year" do
-      @elderberry = FactoryGirl.create(:participant, firstname: "Barry", lastname: "Elder")
+      @someone = FactoryGirl.create(:participant, firstname: "Barry", lastname: "Elder")
 
-      @elderberry.register(@last_year)
+      @someone.register(@last_year)
 
-      expect(@elderberry.years).to include @last_year
-      expect(@elderberry.years).not_to include @this_year
+      expect(@someone.years).to include @last_year
+      expect(@someone.years).not_to include @this_year
     end
   end
 end
