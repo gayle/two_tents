796e42ed0fdfdf6f15d6eb0b4b5a373d81c19182
Gayle Bender <gayle_999@yahoo.com>
2015-02-22 22:21:55 -0500

    troubleshooting migration not working on Heroku

--- db/migrate/20110124010519_create_tables_that_cross_ref_with_years.rb
+++ db/migrate/20110124010519_create_tables_that_cross_ref_with_years.rb
@@ -2,11 +2,14 @@ class CreateTablesThatCrossRefWithYears < ActiveRecord::Migration
   def self.up
     # year_results = execute("SELECT * FROM years ORDER BY year ASC")
     year_results = execute("SELECT * FROM years WHERE year = '2010'")
+    puts "year_results=#{year_results.to_a}"
     if year_results.blank?
       execute %{INSERT INTO years (year) VALUES ('2010')}
       year_results = execute("SELECT * FROM years WHERE year = '2010'")
+      puts "year_results=#{year_results.to_a}"
     end
-    year_2010_id = year_results[0]["id"]
+    first_row = year_results[0]
+    year_2010_id = first_row["id"]
 
     create_table :families_years, :id => false do |t|
       t.integer :family_id
