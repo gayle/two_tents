59e4260a1d6a3d2f4c3621f673f74abc421adc41
Gayle Bender <gayle_999@yahoo.com>
2015-03-01 17:50:15 -0500

    get index page to show login link

--- spec/controllers/content_controller_spec.rb
+++ spec/controllers/content_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ContentController, :type => :controller do
-  describe "GET #index" do
-    it "responds successfully with an HTTP 200 status code" do
+
+  describe "GET index" do
+    it "returns http success" do
       get :index
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:success)
     end
 
     it "renders the index template" do
